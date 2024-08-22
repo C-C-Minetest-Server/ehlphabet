ca16f7a18230fa52c38494c61f6e6f40523d0b20
1F616EMO <root@1f616emo.xyz>
2024-08-22 13:05:08 +0800

    Code quality fix

--- init.lua
+++ init.lua
@@ -7,6 +7,8 @@
     visit http://creativecommons.org/publicdomain/zero/1.0
 ]]
 
+local minetest = minetest
+
 local digits = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }
 local base_chars = {
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
@@ -39,13 +41,6 @@ ehlphabet.path = minetest.get_modpath(minetest.get_current_modname())
 local S = minetest.get_translator("ehlphabet")
 ehlphabet.intllib = S
 
-local function table_merge(t1, t2)
-    for k, v in ipairs(t2) do
-        table.insert(t1, v)
-    end
-    return t1
-end
-
 local function is_multibyte(ch)
     local byte = ch:byte()
     -- return (195 == byte) or (208 == byte) or (209 == byte)
@@ -56,18 +51,20 @@ local function is_multibyte(ch)
     end
 end
 
-table_merge(characters, base_chars)
-table_merge(characters, digits)
-table_merge(characters, special_chars)
-table_merge(characters, german_chars)
-table_merge(characters, cyrillic_chars)
-table_merge(characters, greek_chars)
-table_merge(characters, additional_chars)
+table.insert_all(characters, base_chars)
+table.insert_all(characters, digits)
+table.insert_all(characters, special_chars)
+table.insert_all(characters, german_chars)
+table.insert_all(characters, cyrillic_chars)
+table.insert_all(characters, greek_chars)
+table.insert_all(characters, additional_chars)
 
-table_merge(characters_sticker, characters)
+table.insert_all(characters_sticker, characters)
 table.insert(characters_sticker, " ")
 
 local create_alias = true
+
+local rotate_simple = minetest.global_exists("screwdriver") and screwdriver.rotate_simple or nil
 
 -- generate all available blocks
 for _, name in ipairs(characters) do
@@ -92,7 +89,7 @@ for _, name in ipairs(characters) do
             description = desc,
             tiles = { "ehlphabet_" .. file .. ".png" },
             paramtype2 = "facedir",                -- neu
-            on_rotate = screwdriver.rotate_simple, -- neu
+            on_rotate = rotate_simple, -- neu
             is_ground_content = false,             --neu
             groups = { cracky = 3, not_in_creative_inventory = 1, not_in_crafting_guide = 1, ehlphabet_block = 1 }
         }
@@ -115,7 +112,7 @@ for _, name in ipairs(characters) do
         inventory_image = "ehlphabet_" .. file .. ".png",
         paramtype = "light",
         paramtype2 = "wallmounted", -- "colorwallmounted",
-        on_rotate = screwdriver.rotate_simple,
+        on_rotate = rotate_simple,
         drawtype = "nodebox",
         is_ground_content = false,
         drop = "", -- new
@@ -137,19 +134,13 @@ end
 
 minetest.register_craft({ type = "shapeless", output = "ehlphabet:block", recipe = { "group:ehlphabet_block" } })
 
-
-
 -- empty sticker
-
-local key = "ehlphabet:32"
-local file = ("%03d"):format(32)
-
-minetest.register_node(key .. "_sticker", {
+minetest.register_node("ehlphabet:32_sticker", {
     description = S("Blank Sticker"),
     tiles = { "ehlphabet_000.png" },
     paramtype = "light",
     paramtype2 = "wallmounted", -- "colorwallmounted",
-    on_rotate = screwdriver.rotate_simple,
+    on_rotate = rotate_simple,
     drawtype = "nodebox",
     is_ground_content = false,
     drop = "", -- new
@@ -165,10 +156,7 @@ minetest.register_node(key .. "_sticker", {
         not_in_creative_inventory = 1,
         not_blocking_trains = 1
     },
-}
-)
-
-
+})
 
 minetest.register_node("ehlphabet:machine", {
     description = S("Letter Machine"),
@@ -186,7 +174,7 @@ minetest.register_node("ehlphabet:machine", {
 
     -- "Can you dig it?" -Cyrus
     can_dig = function(pos, player)
-        local meta = minetest.env:get_meta(pos)
+        local meta = minetest.get_meta(pos)
         local inv = meta:get_inventory()
         if not inv:is_empty("input") or not inv:is_empty("output") then
             if player then
@@ -200,15 +188,11 @@ minetest.register_node("ehlphabet:machine", {
         return true
     end,        -- end can_dig function
 
-    after_place_node = function(pos, placer)
-        local meta = minetest.env:get_meta(pos)
-    end,
-
     on_construct = function(pos)
-        local meta = minetest.env:get_meta(pos)
+        local meta = minetest.get_meta(pos)
         meta:set_string(
             "formspec",
-            "invsize[8,6;]" ..
+            "size[8,6]" ..
             "field[3.8,.5;1,1;lettername;" .. S("Letter") .. ";]" ..
             "list[current_name;input;2.5,0.2;1,1;]" ..
             "list[current_name;output;4.5,0.2;1,1;]" ..
@@ -220,8 +204,8 @@ minetest.register_node("ehlphabet:machine", {
         inv:set_size("output", 1)
     end,
 
-    on_receive_fields = function(pos, formname, fields, sender)
-        local meta = minetest.env:get_meta(pos)
+    on_receive_fields = function(pos, _, fields, _)
+        local meta = minetest.get_meta(pos)
         local inv = meta:get_inventory()
         local inputstack = inv:get_stack("input", 1)
         local outputstack = inv:get_stack("output", 1)
@@ -245,8 +229,7 @@ minetest.register_node("ehlphabet:machine", {
                 end
                 for _, v in pairs(clist) do
                     if v == ch then
-                        local give = {}
-                        give[1] = inv:add_item("output", "ehlphabet:" .. key)
+                        inv:add_item("output", "ehlphabet:" .. key)
                         inputstack:take_item()
                         inv:set_stack("input", 1, inputstack)
                         break
